b7688be6742a6f63ca4cffc2a544b78c9df5d8e5
Maguti19 <143103741+Maguti19@users.noreply.github.com>
2023-08-24 10:51:03 -0500

    Update app.py

--- app.py
+++ app.py
@@ -56,7 +56,7 @@ with st.sidebar:
   st.subheader("Configura la modalidad")
   mod_radio = st.radio(
     "Escoge la modalidad a usar",
-    ("Francotirador", "Bomba", "Tranquilizante")
+    ("Audio", "Visual", "Haptico")
   )
                       
 
