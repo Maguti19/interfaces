a2b5ee77826658297cde473615669ab88f742018
Maguti19 <143103741+Maguti19@users.noreply.github.com>
2023-08-24 09:50:17 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,2 +1,7 @@
 import streamlit as st
+from PIL import Image
 st.title('Papers, Please ')
+st. header("Es un juego donde tienes que sellar pasaportes y detener gente")
+st.write("Puedes elegir en que bando jugar")
+
+
